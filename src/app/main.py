e65236762a11eb1d1ff42bd742729c773a0468cf
Karol Kusik <karol22322@gmail.com>
2020-06-13 00:34:36 +0200

    Dodanie elementów aplikacji okienkowej

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,16 @@
 import src.sql.test as api
+import PyQt5.QtGui as QtGui
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
 api.execute()
 api.test("Hiszpania")
+
+
+if __name__ == '__main__':
+    import sys
+    import src.app.modules.start as start
+    app = QApplication(sys.argv)
+    window = start.Start()
+    sys.exit(app.exec_())
