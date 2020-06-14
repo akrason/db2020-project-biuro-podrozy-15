3728945742e3f1224f8d6fda06b0810fe9273e20
Karol Kusik <karol22322@gmail.com>
2020-06-14 18:51:25 +0200

    Update podroze_db.sql

--- src/sql/podroze_db.sql
+++ src/sql/podroze_db.sql
@@ -107,6 +107,7 @@ INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, h
 INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (10, 'Paul', 'Walker', 'czerwonastrzala@onet.re', 665798246, '4f2s');
 INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (11, 'Marie', 'Curie', 'rad@polon.fr', 124563124, '123e');
 INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (13, 'Leonard', 'Euler', 'euler@math.io', 164936253, '1245');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (14, 'Chad', 'Brad', 'ojtakbyczq@plusjeden.pl', 123456969, 'byczek1');
 
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (1, 'Polska', 'Zakopane');
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (2, 'Polska', 'Gdańsk');
@@ -120,6 +121,8 @@ INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (9, 'USA', 'Chi
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (10, 'Kanada', 'Toronto');
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (11, 'Japonia', 'Kioto');
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (12, 'Francja', 'Lyon');
+INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (13, 'Rosja', 'Moskwa');
+INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (14, 'Chorwacja', 'Zagrzeb');
 
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (1, 100.56, 5, 'El Pingüino');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (2, 255.78, 12, 'Paradis');
@@ -134,7 +137,7 @@ INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES 
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (11, 229.00, 4, 'Akacja');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (12, 112.00, 11, 'Khaosan Kyoto');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (13, 126.00, 5, 'Un Patio al Sur');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (14, 681.00, 12, 'Le Royal Lyon');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (14, 682.00, 12, 'Le Royal Lyon');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (15, 329.00, 2, 'Boutique');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (16, 199.00, 6, 'Egnatia');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (17, 205.00, 10, 'The Roncey');
@@ -146,6 +149,7 @@ INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES 
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (23, 160.00, 4, 'TeoDorka');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (24, 60.00, 11, 'Ohto');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (25, 234.56, 9, 'Excellent');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (26, 452.00, 13, 'Gostinica Miedwied');
 
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (1, 750, 'samolot', 'Warszawa');
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (2, 40, 'pociąg', 'Kraków');
@@ -159,19 +163,21 @@ INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transport
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (10, 48.2, 'pociąg', 'Wrocław');
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (11, 0, 'własny', '___');
 
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (1,14*219+2*600,36,7,6,18,'2020-07-03 14:30:00','2020-07-17 19:00:00');
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (2,14*152+2*750,36,8,1,10,'2020-07-10 16:30:00','2020-07-24 09:00:00');
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (3,7*273+2*40,20,3,2,9,'2020-07-03 14:30:00','2020-07-10 19:00:00');
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (4,14*60,46,11,11,24,'2020-07-30 9:30:00','2020-08-13 19:45:00');
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (5,14*168+2*650,36,12,3,14,'2020-07-30 10:30:00','2020-08-13 23:45:00');
-INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (6,14*160,52,4,11,23,'2020-07-30 11:30:00','2020-08-13 08:45:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (1, 4266.00, 34, 7, 6, 18, '2020-07-03 14:30:00', '2020-07-17 19:00:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (2, 3628.00, 36, 8, 1, 10, '2020-07-10 16:30:00', '2020-07-24 09:00:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (3, 1991.00, 20, 3, 2, 9, '2020-07-03 14:30:00', '2020-07-10 19:00:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (4, 840.00, 43, 11, 11, 24, '2020-07-30 09:30:00', '2020-08-13 19:45:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (5, 3652.00, 36, 12, 3, 14, '2020-07-30 10:30:00', '2020-08-13 23:45:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (6, 2240.00, 43, 4, 11, 23, '2020-07-30 11:30:00', '2020-08-13 08:45:00');
+INSERT INTO podroze_db.oferta (id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (7, 4222.00, 55, 13, 1, 26, '2020-06-24 12:30:00', '2020-06-30 14:30:00');
 
-INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (1, 4, '2020-06-14 00:40:42', 0, 11, 2);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (2, 3, '2020-06-14 13:40:22', 0, 3, 4);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (3, 5, '2020-06-03 18:30:15', 0, 4, 3);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (4, 2, '2020-06-05 00:21:42', 0, 8, 2);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (5, 2, '2020-06-14 04:00:00', 0, 10, 6);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (6, 1, '2020-05-26 01:40:48', 0, 1, 5);
 INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (7, 4, '2020-05-17 21:27:42', 0, 5, 3);
+INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (8, 2, '2020-06-14 16:06:05', 1, 11, 1);
+INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (9, 3, '2020-06-14 17:33:27', 0, 11, 4);
 
 COMMIT;
