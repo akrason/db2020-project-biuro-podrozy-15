ba85138ba18b89c30d1ae7b9cb9644ac5a11a02f
Karol Kusik <karol22322@gmail.com>
2020-06-14 02:06:35 +0200

    Modyfikacja pliku sql

--- src/sql/podroze_db.sql
+++ src/sql/podroze_db.sql
@@ -15,7 +15,7 @@ create table klient
     id_klienta  int(6) unsigned auto_increment primary key,
     imie        varchar(30)     not null,
     nazwisko    varchar(30)     not null,
-    email       varchar(50)     not null,
+    email       varchar(50)     unique not null,
     nr_telefonu int(9) unsigned not null,
     haslo varchar(30) not null
 );
@@ -95,16 +95,18 @@ CREATE INDEX id_oferty
     ON rezerwacja (id_oferty);
 
 
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (1, 'Geralt', 'z Rivii', 'rzeznikzBlaviken@gmail.com', 234765894,'1a2s');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (2, 'Triss', 'Merigold', 'czarymary@temeria.org', 123456789, '3d4f');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (3, 'Yennefer', 'z Vengerbergu', 'szybkabryka78@brrrum.com', 859473665,'2s4f');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (4, 'Cirilla', 'Riannon', 'kindersurprajs@muuu.me', 958447337,'3d6h');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (5, 'Radowid', 'Srogi', 'wiecznyogien@onet.re', 684495865,'1a7j');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (6, 'Harry', 'Potter', 'blyskawica15@gmail.com', 152635178,'3d9l');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (7, 'Bilbo', 'Baggins', 'myprecious@interia.org', 185356629,'1a6h');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (8, 'Gandalf', 'Szary', 'ushallnot@pass.com', 965187264,'1a8k');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (9, 'Albus', 'Dumbledore', 'cytrynowe@dropsy.me', 189347512,'2s5g');
-INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu,haslo) VALUES (10, 'Paul', 'Walker', 'czerwonastrzala@onet.re', 665798246, '4f2s');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (1, 'Geralt', 'z Rivii', 'rzeznikzBlaviken@gmail.com', 234765894, '1a2s');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (2, 'Triss', 'Merigold', 'czarymary@temeria.org', 123456789, '3d4f');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (3, 'Yennefer', 'z Vengerbergu', 'szybkabryka78@brrrum.com', 859473665, '2s4f');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (4, 'Cirilla', 'Riannon', 'kindersurprajs@muuu.me', 958447337, '3d6h');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (5, 'Radowid', 'Srogi', 'wiecznyogien@onet.re', 684495865, '1a7j');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (6, 'Harry', 'Potter', 'blyskawica15@gmail.com', 152635178, '3d9l');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (7, 'Bilbo', 'Baggins', 'myprecious@interia.org', 185356629, '1a6h');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (8, 'Gandalf', 'Szary', 'ushallnot@pass.com', 965187264, '1a8k');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (9, 'Albus', 'Dumbledore', 'cytrynowe@dropsy.me', 189347512, '2s5g');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (10, 'Paul', 'Walker', 'czerwonastrzala@onet.re', 665798246, '4f2s');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (11, 'Marie', 'Curie', 'rad@polon.fr', 124563124, '123e');
+INSERT INTO podroze_db.klient (id_klienta, imie, nazwisko, email, nr_telefonu, haslo) VALUES (13, 'Leonard', 'Euler', 'euler@math.io', 164936253, '1245');
 
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (1, 'Polska', 'Zakopane');
 INSERT INTO podroze_db.miejsce (id_miejsca, kraj, miasto) VALUES (2, 'Polska', 'Gdańsk');
@@ -124,25 +126,26 @@ INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES 
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (3, 92.00, 2, 'Smart');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (4, 390.15, 6, 'Poseidon');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (5, 120.24, 10, 'Pied-a-Terre');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (6, 160.0, 7, 'Casena di Colli');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (6, 160.00, 7, 'Casena di Colli');
 INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (7, 95.50, 1, 'PRL');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (8, 262.0, 9, 'Freehand');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (9, 273.0, 3, 'HP Park');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (10, 152.0, 8, 'Passeig de Gracia');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (11, 229.0, 4, 'Akacja');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (12, 112.0, 11, 'Khaosan Kyoto');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (13, 126, 5, 'Un Patio al Sur');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (14, 681, 12, 'Le Royal Lyon');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (15, 329, 2, 'Boutique');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (16, 199, 6, 'Egnatia');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (17, 205, 10, 'The Roncey');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (18, 219, 7, 'Plaza');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (19, 233, 1, 'Biała owca');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (20, 264, 9, 'Found');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (21, 305, 3, 'Przystań');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (22, 199, 8, 'Catalonia Park');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (23, 160, 4, 'TeoDorka');
-INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (24, 60, 11, 'Ohto');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (8, 262.00, 9, 'Freehand');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (9, 273.00, 3, 'HP Park');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (10, 152.00, 8, 'Passeig de Gracia');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (11, 229.00, 4, 'Akacja');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (12, 112.00, 11, 'Khaosan Kyoto');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (13, 126.00, 5, 'Un Patio al Sur');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (14, 681.00, 12, 'Le Royal Lyon');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (15, 329.00, 2, 'Boutique');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (16, 199.00, 6, 'Egnatia');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (17, 205.00, 10, 'The Roncey');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (18, 219.00, 7, 'Plaza');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (19, 233.00, 1, 'Biała owca');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (20, 264.00, 9, 'Found');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (21, 305.00, 3, 'Przystań');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (22, 199.00, 8, 'Catalonia Park');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (23, 160.00, 4, 'TeoDorka');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (24, 60.00, 11, 'Ohto');
+INSERT INTO podroze_db.hotel (id_hotelu, nocleg_cena, id_miejsca, nazwa) VALUES (25, 234.56, 9, 'Excellent');
 
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (1, 750, 'samolot', 'Warszawa');
 INSERT INTO podroze_db.transport (id_transportu, koszt_transportu, typ_transportu, miejsce_wyjazdu) VALUES (2, 40, 'pociąg', 'Kraków');
@@ -161,5 +164,6 @@ INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_tran
 INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (3,7*273+2*40,20,3,2,9,'2020-07-03 14:30:00','2020-07-10 19:00:00');
 INSERT INTO podroze_db.oferta(id_oferty, cena, ilosc_miejsc, id_miejsca, id_transportu, id_hotelu, data_wyjazdu, data_powrotu) VALUES (4,14*199+2*650,46,8,3,22,'2020-07-30 9:30:00','2020-08-13 19:45:00');
 
+INSERT INTO podroze_db.rezerwacja (id_rezerwacji, liczba_osob, data_rezerwacji, platnosc, id_klienta, id_oferty) VALUES (1, 4, '2020-06-14 00:40:42', 0, 11, 2);
  
 COMMIT;
